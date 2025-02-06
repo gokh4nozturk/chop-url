ca6fd76261afe577b32d907a8bd2c15a50ae3211
gokhanozturk <gokhaanozturk@gmail.com>
2025-02-06 18:45:51 +0300

    refactor: update Vitest configuration and test setup
    - Move Vitest setup file to project root
    - Update Vitest config to use new setup file path
    - Remove redundant test setup directory

--- packages/chop-url-fe/vitest.config.ts
+++ packages/chop-url-fe/vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: ['./src/test/setup.ts'],
+    setupFiles: './vitest.setup.ts',
     include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     exclude: ['**/node_modules/**', '**/dist/**'],
     css: true,
