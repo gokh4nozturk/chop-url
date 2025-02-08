61915928c40c949fad63a24e8363f4635526f100
gokhanozturk <gokhaanozturk@gmail.com>
2025-02-08 13:59:59 +0300

    style: remove unnecessary container class

--- packages/chop-url-fe/app/auth/verify-email/page.tsx
+++ packages/chop-url-fe/app/auth/verify-email/page.tsx
@@ -38,7 +38,7 @@ function VerifyEmailContent() {
 
   if (!token) {
     return (
-      <div className="container flex h-screen w-screen flex-col items-center justify-center">
+      <div className="flex h-screen w-screen flex-col items-center justify-center">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <Icons.alertTriangle className="mx-auto h-8 w-8 text-yellow-500" />
@@ -59,7 +59,7 @@ function VerifyEmailContent() {
   }
 
   return (
-    <div className="container flex h-screen w-screen flex-col items-center justify-center">
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
           {isLoading ? (
